ddbd6601077f24f9c8641b290ddf5f97bf4144e1
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2022-12-06 07:32:14 -0500

    Solve day 6 part 2

--- AdventOfCode2022/Day6/Day6.swift
+++ AdventOfCode2022/Day6/Day6.swift
@@ -31,19 +31,40 @@ extension Commands {
                 numberOfCharactersToStartOfPacketMarker,
                 terminator: "\n\n"
             )
+            
+            let numberOfCharactersToStartOfMessageMarker = part2(input: input)
+            printTitle("Part 2", level: .title1)
+            print(
+                "How many characters need to be processed before the first start-of-message marker is detected?",
+                numberOfCharactersToStartOfMessageMarker,
+                terminator: "\n\n"
+            )
         }
         
         func part1(input: String) -> Int {
-            let startOfPacketEndOffset = (4 ..< input.count).first(where: { endOffset in
-                let endIndex = input.index(input.startIndex, offsetBy: endOffset)
-                let startIndex = input.index(endIndex, offsetBy: -4)
-                let range = startIndex ..< endIndex
-                let characters = input[range]
-                let distinctCharacters = Set(characters)
-                return distinctCharacters.count == 4
-            })!
+            let startOfPacketMarker = input.firstRange(withNumberOfDistinctCharacters: 4)!
             
-            return startOfPacketEndOffset
+            return startOfPacketMarker.distance(from: input.startIndex, to: startOfPacketMarker.endIndex)
         }
+        
+        func part2(input: String) -> Int {
+            let startOfMessageMarker = input.firstRange(withNumberOfDistinctCharacters: 14)!
+            
+            return startOfMessageMarker.distance(from: input.startIndex, to: startOfMessageMarker.endIndex)
+        }
+    }
+}
+
+extension String {
+    func firstRange(withNumberOfDistinctCharacters numberOfDistinctCharacters: Int) -> Substring? {
+        indices.dropLast(numberOfDistinctCharacters).lazy
+            .map({ startIndex in
+                let endIndex = index(startIndex, offsetBy: numberOfDistinctCharacters)
+                let range = startIndex ..< endIndex
+                return self[range]
+            })
+            .first(where: { substring in
+                Set(substring).count == numberOfDistinctCharacters
+            })
     }
 }
