c648adc03a5bde6d82a7860eb479638948287392
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2022-12-20 22:22:09 -0500

    Solve day 20 part 2

--- AdventOfCode2022/Day20/Day20.swift
+++ AdventOfCode2022/Day20/Day20.swift
@@ -23,22 +23,29 @@ extension Commands {
         var puzzleInputPath: String
         
         func run() throws {
-            let encryptedFile = try readLines().compactMap(Int.init)
+            let numbers = try readLines().compactMap(Int.init)
             
             printTitle("Part 1", level: .title1)
-            let groveCoordinates = part1(encryptedFile: encryptedFile)
+            let groveCoordinates = part1(numbers: numbers)
             print(
                 "Mix your encrypted file exactly once. What is the sum of the three numbers that form the grove coordinates?",
                 groveCoordinates,
                 terminator: "\n\n"
             )
+            
+            printTitle("Part 2", level: .title1)
+            let groveCoordinatesWithEncryptionKey = part2(numbers: numbers)
+            print(
+                "Apply the decryption key and mix your encrypted file ten times. What is the sum of the three numbers that form the grove coordinates?",
+                groveCoordinatesWithEncryptionKey
+            )
         }
         
-        func part1(encryptedFile: [Int]) -> Int {
-            let count = encryptedFile.count
-            var indices = Deque(encryptedFile.indices)
+        func part1(numbers: [Int]) -> Int {
+            let count = numbers.count
+            var indices = Deque(numbers.indices)
             
-            for (index, value) in encryptedFile.enumerated() {
+            for (index, value) in numbers.enumerated() {
                 let indexOfIndex = indices.firstIndex(of: index)!
                 indices.remove(at: indexOfIndex)
                 
@@ -50,10 +57,39 @@ extension Commands {
                 indices.insert(index, at: insertionIndex)
             }
             
-            let mixedIndexOfZero = indices.firstIndex(of: encryptedFile.firstIndex(of: 0)!)!
+            let mixedIndexOfZero = indices.firstIndex(of: numbers.firstIndex(of: 0)!)!
             let groveCoordinates = [1_000, 2_000, 3_000].reduce(into: 0, { sum, offset in
                 let indicesIndex = (mixedIndexOfZero + offset) % count
-                let value = encryptedFile[indices[indicesIndex]]
+                let value = numbers[indices[indicesIndex]]
+                sum += value
+            })
+            return groveCoordinates
+        }
+        
+        func part2(numbers: [Int]) -> Int {
+            let decryptionKey = 811589153
+            let numbers = numbers.map({ decryptionKey * $0 })
+            let count = numbers.count
+            var indices = Deque(numbers.indices)
+            
+            for _ in 0 ..< 10 {
+                for (index, value) in numbers.enumerated() {
+                    let indexOfIndex = indices.firstIndex(of: index)!
+                    indices.remove(at: indexOfIndex)
+                    
+                    var insertionIndex = (indexOfIndex + value) % (count - 1)
+                    if insertionIndex < 0 {
+                        insertionIndex += count - 1
+                    }
+                    
+                    indices.insert(index, at: insertionIndex)
+                }
+            }
+            
+            let mixedIndexOfZero = indices.firstIndex(of: numbers.firstIndex(of: 0)!)!
+            let groveCoordinates = [1_000, 2_000, 3_000].reduce(into: 0, { sum, offset in
+                let indicesIndex = (mixedIndexOfZero + offset) % count
+                let value = numbers[indices[indicesIndex]]
                 sum += value
             })
             return groveCoordinates
