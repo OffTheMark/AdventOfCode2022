5ffc079108ae49e67595f8c6262627fc6c91a04d
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2022-12-23 10:41:43 -0500

    Solve day 21 part 2

--- AdventOfCode2022/Day21/Day21.swift
+++ AdventOfCode2022/Day21/Day21.swift
@@ -24,12 +24,11 @@ extension Commands {
         
         func run() throws {
             let jobsByMonkey: [String: MonkeyJob] = try readLines().reduce(into: [:], { result, line in
-                let components = line.components(separatedBy: ": ")
-                guard components.count == 2, let job = MonkeyJob(rawValue: components[1]) else {
+                guard let job = MonkeyJob(rawValue: line) else {
                     return
                 }
                 
-                result[components[0]] = job
+                result[job.name] = job
             })
             
             printTitle("Part 1", level: .title1)
@@ -49,227 +48,210 @@ extension Commands {
         }
         
         fileprivate func part1(jobsByMonkey: [String: MonkeyJob]) -> Int {
-            value(forMonkey: "root", in: jobsByMonkey)
+            value(for: jobsByMonkey["root"]!, in: jobsByMonkey)
         }
         
         fileprivate func part2(jobsByMonkey: [String: MonkeyJob]) -> Int {
-            var jobsByMonkey = jobsByMonkey
-            jobsByMonkey.removeValue(forKey: "humn")
             let jobForRoot = jobsByMonkey["root"]!
             
-            guard case .mathOperation(_, let lhs, let rhs) = jobForRoot else {
+            guard case .mathOperation(_, let lhs, let rhs) = jobForRoot.action else {
                 fatalError(#"Monket "root"'s job is not a math operation."#)
             }
             
-            let lhsDependencies = dependencies(forMonkey: lhs, in: jobsByMonkey)
-            
-            let monkeyDependingOnHuman: String
-            let valueOfOtherMonkey: Int
-            if lhsDependencies.contains("humn") {
-                monkeyDependingOnHuman = lhs
-                valueOfOtherMonkey = value(forMonkey: rhs, in: jobsByMonkey)
-            }
-            else {
-                monkeyDependingOnHuman = rhs
-                valueOfOtherMonkey = value(forMonkey: lhs, in: jobsByMonkey)
-            }
-            
-            var monkeyJobsInRelationToHuman: [String: MonkeyJob] = [monkeyDependingOnHuman: .value(valueOfOtherMonkey)]
-            var queue: Deque = [monkeyDependingOnHuman, "humn"]
-            var visited = Set<String>()
-            
-            while let current = queue.popFirst() {
-                if visited.contains(current) {
-                    continue
-                }
-                visited.insert(current)
-                
-                if let job = jobsByMonkey[current], case .value = job {
-                    monkeyJobsInRelationToHuman[current] = job
-                    continue
+            func depthFirstSearchPath(jobsByMonkey: [String: MonkeyJob], start: String, goal: String) -> [MonkeyJob] {
+                struct State {
+                    let job: MonkeyJob
+                    let path: [MonkeyJob]
                 }
                 
-                let pairsDependingOnCurrent = jobsByMonkey.filter({ $1.dependencies.contains(current) })
-                for pairDependingOnCurrent in pairsDependingOnCurrent {
-                    let terms = pairDependingOnCurrent.value.terms!
-                    let correctedTerms = termsOfOperation(
-                        terms.operation,
-                        result: pairDependingOnCurrent.key,
-                        lhs: terms.lhs,
-                        rhs: terms.rhs,
-                        inRelationTo: current
-                    )
-                    let job: MonkeyJob = .mathOperation(
-                        operation: correctedTerms.operation,
-                        lhs: correctedTerms.lhs,
-                        rhs: correctedTerms.rhs
-                    )
-                    monkeyJobsInRelationToHuman[current] = job
-                    
-                    let nextInQueue = job.dependencies.filter({ !visited.contains($0) })
-                    queue.append(contentsOf: nextInQueue)
-                }
-            }
-            
-            return value(forMonkey: "humn", in: monkeyJobsInRelationToHuman)
-        }
-        
-        private func value(forMonkey target: String, in jobsByMonkey: [String: MonkeyJob]) -> Int {
-            var monkeysToConfirm = dependencies(forMonkey: target, in: jobsByMonkey).union([target])
-            var confirmedValuesByMonkey = [String: Int]()
-            
-            while !monkeysToConfirm.isEmpty {
-                for monkey in monkeysToConfirm {
-                    let isValueConfirmed: Bool
-                    switch jobsByMonkey[monkey] {
-                    case .value(let value):
-                        confirmedValuesByMonkey[monkey] = value
-                        isValueConfirmed = true
-                        
-                    case .mathOperation(let operation, let lhsMonkey, let rhsMonkey):
-                        if let lhs = confirmedValuesByMonkey[lhsMonkey], let rhs = confirmedValuesByMonkey[rhsMonkey] {
-                            let value = operation.calculate(lhs: lhs, rhs: rhs)
-                            confirmedValuesByMonkey[monkey] = value
-                            isValueConfirmed = true
-                        }
-                        else {
-                            isValueConfirmed = false
-                        }
-                        
-                    case nil:
+                let startJob = jobsByMonkey[start]!
+                var stack: Deque<State> = [.init(job: startJob, path: [startJob])]
+                var visited = Set<MonkeyJob>()
+                
+                while let current = stack.popLast() {
+                    guard !visited.contains(current.job) else {
                         continue
                     }
                     
-                    if isValueConfirmed {
-                        monkeysToConfirm.remove(monkey)
+                    if current.job.name == goal {
+                        return current.path
+                    }
+                    
+                    visited.insert(current.job)
+                    
+                    for neighbor in jobsByMonkey[current.job.name]!.action.dependencies {
+                        let neighborJob = jobsByMonkey[neighbor]!
+                        stack.append(.init(job: neighborJob, path: current.path + [neighborJob]))
+                    }
+                }
+                
+                fatalError(#"Could not find path from "\#(start)" to "\#(goal)""#)
+            }
+            
+            let path = depthFirstSearchPath(jobsByMonkey: jobsByMonkey, start: "root", goal: "humn")
+            let distinctJobsInPath = Set(path)
+            
+            func humanNumber(job: MonkeyJob, result: Int) -> Int {
+                if job.name == "humn" {
+                    return result
+                }
+                
+                switch job.action {
+                case .value:
+                    fatalError("Could not find human number")
+                    
+                case .mathOperation(let operation, let lhs, let rhs):
+                    let leftJob = jobsByMonkey[lhs]!
+                    let rightJob = jobsByMonkey[rhs]!
+                    if distinctJobsInPath.contains(leftJob) {
+                        return humanNumber(
+                            job: leftJob,
+                            result: operation.invertLeft(
+                                rhs: value(
+                                    for: rightJob,
+                                    in: jobsByMonkey
+                                ),
+                                result: result
+                            )
+                        )
+                    }
+                    else {
+                        return humanNumber(
+                            job: rightJob,
+                            result: operation.invertRight(
+                                lhs: value(
+                                    for: leftJob,
+                                    in: jobsByMonkey
+                                ),
+                                result: result
+                            )
+                        )
                     }
                 }
             }
             
-            return confirmedValuesByMonkey[target]!
+            let leftJob = jobsByMonkey[lhs]!
+            let rightJob = jobsByMonkey[rhs]!
+            if path.contains(leftJob) {
+                let targetValue = value(for: rightJob, in: jobsByMonkey)
+                return humanNumber(job: leftJob, result: targetValue)
+            }
+            else {
+                let targetValue = value(for: jobsByMonkey[lhs]!, in: jobsByMonkey)
+                return humanNumber(job: rightJob, result: targetValue)
+            }
         }
         
-        private func dependencies(forMonkey target: String, in jobsByMonkey: [String: MonkeyJob]) -> Set<String> {
-            guard let jobForTarget = jobsByMonkey[target] else {
-                return []
-            }
-            
-            var dependencies = Set<String>()
-            var queue = Deque<String>()
-            switch jobForTarget {
-            case .mathOperation:
-                queue.append(contentsOf: jobForTarget.dependencies)
+        private func value(for monkeyJob: MonkeyJob, in jobsByMonkey: [String: MonkeyJob]) -> Int {
+            switch monkeyJob.action {
+            case .value(let value):
+                return value
                 
-            case .value:
-                return []
-            }
-            
-            while let current = queue.popFirst() {
-                dependencies.insert(current)
-                
-                if let currentJob = jobsByMonkey[current] {
-                    let nextInQueue = currentJob.dependencies.filter({ !dependencies.contains($0) })
-                    queue.append(contentsOf: nextInQueue)
-                }
-            }
-            
-            return dependencies
-        }
-        
-        private func termsOfOperation(
-            _ operation: MonkeyJob.MathOperation,
-            result: String,
-            lhs: String,
-            rhs: String,
-            inRelationTo targetMonkey: String
-        ) -> MonkeyJob.Terms {
-            if targetMonkey == result {
-                return (operation, lhs, rhs)
-            }
-            
-            switch operation {
-            case .add:
-                if targetMonkey == lhs {
-                    return (.subtract, result, rhs)
-                }
-                else {
-                    return (.subtract, result, lhs)
-                }
-                
-            case .subtract:
-                if targetMonkey == lhs {
-                    return (.add, result, rhs)
-                }
-                else {
-                    return (.subtract, lhs, result)
-                }
-                
-            case .multiply:
-                if targetMonkey == lhs {
-                    return (.divide, result, rhs)
-                }
-                else {
-                    return (.divide, result, lhs)
-                }
-                
-            case .divide:
-                if targetMonkey == lhs {
-                    return (.multiply, result, rhs)
-                }
-                else {
-                    return (.divide, lhs, result)
-                }
+            case .mathOperation(let operation, let lhs, let rhs):
+                return operation.calculate(
+                    lhs: value(for: jobsByMonkey[lhs]!, in: jobsByMonkey),
+                    rhs: value(for: jobsByMonkey[rhs]!, in: jobsByMonkey)
+                )
             }
         }
     }
 }
 
-fileprivate enum MonkeyJob {
-    case value(Int)
-    case mathOperation(
-        operation: MathOperation,
-        lhs: String,
-        rhs: String
-    )
+fileprivate struct MonkeyJob: Hashable {
+    let name: String
+    let action: Action
     
-    var dependencies: Set<String> {
-        switch self {
-        case .mathOperation(_, let lhs, let rhs):
-            return [lhs, rhs]
-            
+    func rotatedLeft() -> Self {
+        switch action {
         case .value:
-            return []
+            return self
+            
+        case .mathOperation(let operation, let lhs, let rhs):
+            switch operation {
+            case .add:
+                return Self(name: lhs, action: .mathOperation(operation: .subtract, lhs: name, rhs: rhs))
+                
+            case .subtract:
+                return Self(name: lhs, action: .mathOperation(operation: .add, lhs: name, rhs: rhs))
+                
+            case .multiply:
+                return Self(name: lhs, action: .mathOperation(operation: .divide, lhs: name, rhs: rhs))
+                
+            case .divide:
+                return Self(name: lhs, action: .mathOperation(operation: .multiply, lhs: name, rhs: rhs))
+            }
         }
     }
     
-    var terms: Terms? {
-        switch self {
-        case .mathOperation(let operation, let lhs, let rhs):
-            return (operation, lhs, rhs)
-            
+    func rotatedRight() -> Self {
+        switch action {
         case .value:
-            return nil
+            return self
+            
+        case .mathOperation(let operation, let lhs, let rhs):
+            switch operation {
+            case .add:
+                return Self(name: rhs, action: .mathOperation(operation: .subtract, lhs: name, rhs: lhs))
+                
+            case .subtract:
+                return Self(name: rhs, action: .mathOperation(operation: .subtract, lhs: lhs, rhs: name))
+                
+            case .multiply:
+                return Self(name: rhs, action: .mathOperation(operation: .divide, lhs: name, rhs: lhs))
+                
+            case .divide:
+                return Self(name: rhs, action: .mathOperation(operation: .divide, lhs: lhs, rhs: name))
+            }
+        }
+    }
+    
+    enum Action: Hashable {
+        case value(Int)
+        case mathOperation(
+            operation: MathOperation,
+            lhs: String,
+            rhs: String
+        )
+        
+        init?(rawValue: String) {
+            let components = rawValue.components(separatedBy: " ")
+            
+            if components.count == 1, let value = Int(components[0]) {
+                self = .value(value)
+                return
+            }
+            
+            guard components.count == 3, let operation = MathOperation(rawValue: components[1]) else {
+                return nil
+            }
+            
+            self = .mathOperation(operation: operation, lhs: components[0], rhs: components[2])
+        }
+        
+        var dependencies: Set<String> {
+            switch self {
+            case .mathOperation(_, let lhs, let rhs):
+                return [lhs, rhs]
+                
+            case .value:
+                return []
+            }
+        }
+        
+        var terms: Terms? {
+            switch self {
+            case .mathOperation(let operation, let lhs, let rhs):
+                return (operation, lhs, rhs)
+                
+            case .value:
+                return nil
+            }
         }
     }
     
     typealias Terms = (operation: MathOperation, lhs: String, rhs: String)
     
-    init?(rawValue: String) {
-        let components = rawValue.components(separatedBy: " ")
-        
-        if components.count == 1, let value = Int(components[0]) {
-            self = .value(value)
-            return
-        }
-        
-        guard components.count == 3, let operation = MathOperation(rawValue: components[1]) else {
-            return nil
-        }
-        
-        self = .mathOperation(operation: operation, lhs: components[0], rhs: components[2])
-    }
-    
-    enum MathOperation: String {
+    enum MathOperation: String, Hashable {
         case add = "+"
         case subtract = "-"
         case multiply = "*"
@@ -290,5 +272,49 @@ fileprivate enum MonkeyJob {
                 return lhs / rhs
             }
         }
+        
+        func invertLeft(rhs: Int, result: Int) -> Int {
+            switch self {
+            case .add:
+                return result - rhs
+            
+            case .subtract:
+                return result + rhs
+                
+            case .multiply:
+                return result / rhs
+                
+            case .divide:
+                return result * rhs
+            }
+        }
+        
+        func invertRight(lhs: Int, result: Int) -> Int {
+            switch self {
+            case .add:
+                return result - lhs
+            
+            case .subtract:
+                return lhs - result
+                
+            case .multiply:
+                return result / lhs
+                
+            case .divide:
+                return lhs / result
+            }
+        }
+    }
+}
+
+fileprivate extension MonkeyJob {
+    init?(rawValue: String) {
+        let components = rawValue.components(separatedBy: ": ")
+        guard components.count == 2, let action = Action(rawValue: components[1]) else {
+            return nil
+        }
+        
+        self.name = components[0]
+        self.action = action
     }
 }
